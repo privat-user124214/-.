fe75a76740e9faafa3fb92f5a4f1414f009cfc59
user-z55554dfgfh <novarix.studio@gmail.com>
2025-01-22 12:48:05 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,7 +1,11 @@
-// Burger Menü für die Navigation
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const burger = document.querySelector('.burger');
+        const navLinks = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+        burger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            burger.classList.toggle('toggle');
+        });
+    });
+</script>
