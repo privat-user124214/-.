721b2a6ffb02c4af3bcf12df9b2ed6302200e6b4
user-z55554dfgfh <novarix.studio@gmail.com>
2025-03-22 03:52:20 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,11 +1,9 @@
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        const burger = document.querySelector('.burger');
-        const navLinks = document.querySelector('.nav-links');
+// JavaScript für das Burger-Menü
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav ul');
 
-        burger.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-            burger.classList.toggle('toggle');
-        });
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('active');
     });
-</script>
+});
